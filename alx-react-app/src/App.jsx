011dd786b16fe2edda5207d0e64f1cfee4bbc59f
Daniel Komolafe <daniel@komolafe.dev>
2026-01-06 21:37:10 -0600

    finished task

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <Header />
       <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photograpy" />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   );
