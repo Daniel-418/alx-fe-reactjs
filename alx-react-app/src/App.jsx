8ed1d128ed00bec79bb395846584a3730dc74036
Daniel Komolafe <daniel@komolafe.dev>
2026-01-06 21:36:39 -0600

    finished task

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile.jsx'
 
 function App() {
 
@@ -13,6 +11,7 @@ function App() {
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photograpy" />
       <Footer />
     </>
   );
