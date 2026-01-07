bbdc0364d8bd8e322bd6144555c5666fc9a4c2c2
Daniel Komolafe <daniel@komolafe.dev>
2026-01-06 21:30:40 -0600

    finished task

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './Header';
-import Footer from './Footer';
-import MainContent from './MainContent'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
