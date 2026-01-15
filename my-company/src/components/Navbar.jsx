dbc94ef465d6143b302c636b009b904b6b7b3099
Daniel Komolafe <daniel@komolafe.dev>
2026-01-14 19:04:54 -0600

    finished task

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,12 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const linkStyle = {
-    color: 'white',
     textDecoration: 'none',
     margin: '0 10px' // Adds a little space between links
   };
   return (<div>
-    <ul>
+    <ul style={{
+      backgroundColor: 'lightgray',
+      display: 'flex',
+      justifyContent: 'center'
+    }}>
       <li style={{
         color: 'white'
       }}>
