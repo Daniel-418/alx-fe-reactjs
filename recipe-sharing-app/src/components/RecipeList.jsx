0e8ed1779047e839fcd9803fab9b8bbaa5a27fc7
Daniel Komolafe <daniel@komolafe.dev>
2026-01-23 23:45:33 -0600

    finished task

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import useRecipeStore from "../recipeStore";
+import useRecipeStore from "./recipeStore";
 
 export default function RecipeList() {
   const recipes = useRecipeStore(state => state.recipes);
