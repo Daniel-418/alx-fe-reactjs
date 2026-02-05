b7d9c56023d8c73f220f44c98dc0812351ec108b
Daniel Komolafe <daniel@komolafe.dev>
2026-02-04 20:23:29 -0600

    finished task

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img className="rounded-full w-36 h-36 mx-auto" src="https://placehold.co/150" alt="User" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="user-profile bg-gray-100 p-2 md:p-8 sm:p-4 max-w-sm mx-auto my-20 rounded-lg shadow-lg md:max-w-sm sm:max-w-xs">
+      <img className="rounded-full md:w-36 md:h-36 w-24 h-24 mx-auto" src="https://placehold.co/150" alt="User" />
+      <h1 className="md:text-xl text-sm text-blue-800 my-4">John Doe</h1>
+      <p className="md:text-base text-sm text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
